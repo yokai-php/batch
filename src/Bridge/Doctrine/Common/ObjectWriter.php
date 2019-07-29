6efcf389392f415b04f3abe010fd5f6108d91fa1
Yann Eugoné <yeugone@prestaconcept.net>
2019-07-29 10:40:41 +0200

    Enhance doctrine ObjectWriter to store manager for class in local registry

--- src/Bridge/Doctrine/Common/ObjectWriter.php
+++ src/Bridge/Doctrine/Common/ObjectWriter.php
@@ -18,6 +18,11 @@ final class ObjectWriter implements ItemWriterInterface
      */
     private $encounteredManagers = [];
 
+    /**
+     * @var ObjectManager[]
+     */
+    private $managerForClass = [];
+
     public function __construct(ManagerRegistry $doctrine)
     {
         $this->doctrine = $doctrine;
@@ -29,10 +34,7 @@ final class ObjectWriter implements ItemWriterInterface
     public function write(iterable $items): void
     {
         foreach ($items as $item) {
-            $manager = $this->getManager($item);
-            if (!$manager->contains($item)) {
-                $manager->persist($item);
-            }
+            $this->getManagerForClass($item)->persist($item);
         }
 
         foreach ($this->encounteredManagers as $manager) {
@@ -40,35 +42,43 @@ final class ObjectWriter implements ItemWriterInterface
         }
 
         foreach ($items as $item) {
-            $this->getManager($item)->detach($item);
+            $this->getManagerForClass($item)->detach($item);
         }
 
         $this->encounteredManagers = [];
     }
 
-    private function getManager($item): ObjectManager
+    private function getManagerForClass($item): ObjectManager
     {
-        $invalidItemException = new \LogicException(
+        if (!is_object($item)) {
+            throw $this->createInvalidItemException($item);
+        }
+
+        $class = get_class($item);
+        $manager = $this->managerForClass[$class] ?? null;
+        if ($manager instanceof ObjectManager) {
+            $this->encounteredManagers[spl_object_id($manager)] = $manager;
+
+            return $this->managerForClass[$class];
+        }
+
+        $manager = $this->doctrine->getManagerForClass($class);
+        if ($manager === null) {
+            throw $this->createInvalidItemException($item);
+        }
+
+        $this->managerForClass[$class] = $manager;
+
+        return $manager;
+    }
+
+    private function createInvalidItemException($item): \LogicException
+    {
+        return new \LogicException(
             sprintf(
                 'Items to write must be object managed by Doctrine. Got "%s".',
                 is_object($item) ? get_class($item) : gettype($item)
             )
         );
-
-        if (!is_object($item)) {
-            throw $invalidItemException;
-        }
-
-        $manager = $this->doctrine->getManagerForClass(get_class($item));
-        if ($manager === null) {
-            throw $invalidItemException;
-        }
-
-        $managerId = spl_object_hash($manager);
-        if (!isset($this->encounteredManagers[$managerId])) {
-            $this->encounteredManagers[$managerId] = $manager;
-        }
-
-        return $manager;
     }
 }
