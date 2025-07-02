7917690ac8ab280d192d417b62ceb0f8bb0ef182
ARDINStephane <33199763+ARDINStephane@users.noreply.github.com>
2025-07-02 11:43:39 +0200

    Add access to JobExecution in CallbackWriter (#173)
    Co-authored-by: sardin <sardin@prestaconcept.net>

--- src/Job/Item/Writer/CallbackWriter.php
+++ src/Job/Item/Writer/CallbackWriter.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace Yokai\Batch\Job\Item\Writer;
 
 use Yokai\Batch\Job\Item\ItemWriterInterface;
+use Yokai\Batch\Job\JobExecutionAwareInterface;
+use Yokai\Batch\Job\JobExecutionAwareTrait;
 
 /**
  * An {@see ItemWriterInterface} that write items with a {@see Closure} provided at construction.
  *
  * Provided {@see Closure} must accept items to write and must return nothing.
  */
-final class CallbackWriter implements ItemWriterInterface
+final class CallbackWriter implements ItemWriterInterface, JobExecutionAwareInterface
 {
+    use JobExecutionAwareTrait;
+
     public function __construct(
         private \Closure $callback,
     ) {
@@ -20,6 +24,6 @@ final class CallbackWriter implements ItemWriterInterface
 
     public function write(iterable $items): void
     {
-        ($this->callback)($items);
+        ($this->callback)($items, $this->jobExecution);
     }
 }
--- tests/Job/Item/Writer/CallbackWriterTest.php
+++ tests/Job/Item/Writer/CallbackWriterTest.php
@@ -6,18 +6,27 @@ namespace Yokai\Batch\Tests\Job\Item\Writer;
 
 use PHPUnit\Framework\TestCase;
 use Yokai\Batch\Job\Item\Writer\CallbackWriter;
+use Yokai\Batch\JobExecution;
 
 class CallbackWriterTest extends TestCase
 {
     public function testWrite(): void
     {
         $saveditems = [];
-        $writer = new CallbackWriter(function (array $items) use (&$saveditems) {
-            $saveditems = [...$saveditems, ...$items];
-        });
+        $writer = new CallbackWriter(
+            function (array $items, JobExecution $jobExecution) use (&$saveditems, &$receivedJobExecution) {
+                $saveditems = [...$saveditems, ...$items];
+                $receivedJobExecution = $jobExecution;
+            },
+        );
+
+        $writer->setJobExecution(JobExecution::createRoot(id: '123', jobName: 'test'));
         $writer->write([1, 2, 3]);
         $writer->write([4, 5, 6]);
 
         self::assertSame([1, 2, 3, 4, 5, 6], $saveditems);
+        self::assertInstanceOf(JobExecution::class, $receivedJobExecution);
+        self::assertSame('123', $receivedJobExecution->getId());
+        self::assertSame('test', $receivedJobExecution->getJobName());
     }
 }
